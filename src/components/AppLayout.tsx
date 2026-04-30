1cb1a415c17d05a557cd42f9d93fd09bb0316491
manoj7ar <manoj7ar@gmail.com>
2026-04-30 12:49:14 +0100

    feat(layout): render PerformanceHud when setting enabled (2/2)

--- src/components/AppLayout.tsx
+++ src/components/AppLayout.tsx
@@ -44,6 +44,7 @@ export function AppLayout({ children }: PropsWithChildren) {
         <RelayPausedBanner />
         {children}
       </main>
+      <PerformanceHud />
       <BottomNav />
     </div>
   );
